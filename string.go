3a938c60e4831f68f1774d03147015e33a73f335
Danya Sliusar <dsliusar@wimarksystems.com>
2020-08-26 17:37:42 +0300

    Fix: possible panic on Store into nil map

--- string.go
+++ string.go
@@ -15,12 +15,11 @@ func (m *String) Load(key string) bool {
 	if key == "" {
 		return false
 	}
-	if m.d == nil {
-		m.d = map[string]void{}
-		return false
-	}
 	m.m.RLock()
 	defer m.m.RUnlock()
+	if m.d == nil {
+		return false
+	}
 	_, ok := m.d[key]
 	return ok
 }
@@ -30,11 +29,11 @@ func (m *String) Store(key string) {
 	if key == "" {
 		return
 	}
+	m.m.Lock()
+	defer m.m.Unlock()
 	if m.d == nil {
 		m.d = map[string]void{}
 	}
-	m.m.Lock()
-	defer m.m.Unlock()
 	m.d[key] = void{}
 }
 
@@ -43,23 +42,21 @@ func (m *String) Delete(key string) {
 	if key == "" {
 		return
 	}
-	if m.d == nil {
-		m.d = map[string]void{}
-		return
-	}
 	m.m.Lock()
 	defer m.m.Unlock()
+	if m.d == nil {
+		return
+	}
 	delete(m.d, key)
 }
 
 // Keys for return all keys in set
 func (m *String) Keys() []string {
+	m.m.RLock()
+	defer m.m.RUnlock()
 	if m.d == nil {
 		return []string{}
 	}
-
-	m.m.Lock()
-	defer m.m.Unlock()
 
 	keys := make([]string, 0, len(m.d))
 	for k := range m.d {
